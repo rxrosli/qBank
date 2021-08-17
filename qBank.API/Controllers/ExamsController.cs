39d240cb04e106cceb1b42c2e2e5c0d0a6adc4f8
Raymark Roslin <raymark.roslin@gmail.com>
2021-08-17 08:04:47 +0800

    Add Exam and Question Controller.

---
+++ qBank.API/Controllers/ExamsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using qBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace qBankAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExamsController : ControllerBase
+    {
+        // GET api/exams
+        [HttpGet]
+        public IEnumerable<Exam> Get()
+        {
+            return new List<Exam>();
+        }
+
+        // GET api/exams/{id}
+        [HttpGet("{id}")]
+        public Exam Get(String id)
+        {
+            return new Exam();
+        }
+
+        // POST api/exams
+        [HttpPost]
+        public void Post([FromBody] Exam value)
+        {
+
+        }
+
+        // PUT api/exams/{id}
+        [HttpPut("{id}")]
+        public void Put(String id, [FromBody] Exam value)
+        {
+
+        }
+
+        // DELETE api/exams/{id}
+        [HttpDelete("{id}")]
+        public void Delete(String id)
+        {
+
+        }
+    }
+}
---
+++ qBank.API/Controllers/QuestionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using qBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace qBankAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        // GET: api/questions
+        [HttpGet]
+        public IEnumerable<Question> Get()
+        {
+            return new List<Question>();
+        }
+
+        // GET api/questions/5
+        [HttpGet("{id}")]
+        public Question Get(String id)
+        {
+            return new Question();
+        }
+
+        // POST api/questions
+        [HttpPost]
+        public void Post([FromBody] Question value)
+        {
+        }
+
+        // PUT api/questions/5
+        [HttpPut("{id}")]
+        public void Put(String id, [FromBody] Question value)
+        {
+        }
+
+        // DELETE api/questions/5
+        [HttpDelete("{id}")]
+        public void Delete(String id)
+        {
+        }
+    }
+}
