05aeedda9cafbba60f48d842889a0b1fcee1bf7b
Raymark Roslin <raymark.roslin@gmail.com>
2021-08-17 11:02:41 +0800

    Add QbankContext.

---
+++ qBank.API/Data/QBankContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using qBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qBank.API.Data
+{
+    public class SqliteContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+
+        public string DbPath { get; private set; }
+
+        public SqliteContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}qBank.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
